d705fe151646e8e977e5edbbb9d0a8dd2bf42f3f
brianleroux <b@brian.io>
2016-04-03 14:47:41 -0700

    add filter to list

--- scripts/list.js
+++ scripts/list.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
+
 var aws = require('aws-sdk')
-var region = process.argv[2] || 'us-east-1'
+var region = process.env.AWS_REGION || 'us-east-1'
 var lambda = new aws.Lambda({region:region})
 var chalk = require('chalk')
+var startsWith = require('lodash').startsWith
+var filtering = process.argv[2] || ''
 
+function log(txt) {
+  console.log(chalk.green(' λ ') + chalk.cyan.underline.dim(txt))
+}
 lambda.listFunctions({}, (err, fns)=> {
-  var names = fns.Functions.map(f=> f.FunctionName).sort()
-  names.forEach(name=> {
-    console.log(chalk.green(' λ ') + chalk.yellow(name))
-  })
+  console.log(chalk.green(' λ ') + chalk.dim.grey('listing deployed lambdas' ))
+  var names = fns.Functions.map(f=> f.FunctionName).sort().filter(name=> startsWith(name, filtering))
+  names.forEach(log)
+  console.log(chalk.green(' λ '))
 })
