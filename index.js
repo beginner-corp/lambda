9c69bf1e58a3708dc0953640feb8cf620633b2eb
Brian LeRoux <b@brian.io>
2017-05-18 09:07:27 -0700

    add transition fn

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var lambda = require('./_lambda')
 var dynamo = require('./_dynamo')
 var local = require('./_local') 
 var sns = require('./_sns')
+var json = require('./_arc')
 
 lambda.local = local
 lambda.triggers = {dynamo, sns}
+lambda.arc = {}
+lambda.arc.json = json
 
 module.exports = lambda
